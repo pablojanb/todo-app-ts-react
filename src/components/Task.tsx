ac31a554ba99816c74a82d58ea69cc06f58564e1
pabloj <pablojanb@gmail.com>
2025-02-05 00:43:36 -0300

    add toCapital fn

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,13 +1,14 @@
+import { toCapital } from "../utils/toCapital"
+
 type Task = {
     task: string
     deleteTask: ()=>void
 }
 
-
 export const Task = ({task, deleteTask}: Task)=>{
     return (
         <div className="task">
-            <span>{task}</span>
+            <span>{toCapital(task)}</span>
             <button className="btnDelete" onClick={deleteTask}>Delete</button>
         </div>
     )
