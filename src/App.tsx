2514a553ead0427876bb315a5aa7ce0a2c50a61a
pabloj <pablojanb@gmail.com>
2025-02-05 16:53:56 -0300

    add tasks counter

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,19 @@ function App() {
 
   const [task, setTask] = useState<string>("")
   const [tasksList, setTasksList] = useState<string[]>(initialTaskList)
+  const [taskCounter, setTaskCounter] = useState(tasksList.length)
 
   const handleAddTask = () =>{
     if (task.trim() !== "") {
       setTasksList(tasksList => [...tasksList, task])
+      setTaskCounter(prev=> prev + 1)
       setTask("")
     }
   }
 
   const handleDeleteTask = (index: number)=> {
     setTasksList(tasksList=> tasksList.filter((_, i) => i !== index))
+    setTaskCounter(prev=> prev - 1)
   }
 
   useEffect(()=>{
@@ -27,7 +30,7 @@ function App() {
 
   return (
     <div className='container'>
-      <h1 className='title'>To Do List</h1>
+      <h1 className='title'>To Do List {taskCounter !== 0 && `(${taskCounter})`}</h1>
       <input className= 'newTask inputTask' type="text" 
       placeholder='New task'
       value={task}
